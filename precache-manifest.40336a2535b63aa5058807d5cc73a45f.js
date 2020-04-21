81669326f22ce884a386aa20843c9987857f525d
BrianPulfer <pulfer.brian21@gmail.com>
2020-04-21 19:56:15 +0200

    Updates

---
+++ precache-manifest.40336a2535b63aa5058807d5cc73a45f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9891bc42246a3fa73e31cee9181c6d",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "7fae0e01c79f56f2f03f",
+    "url": "/website/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "68f431c9fd3b9892595c",
+    "url": "/website/static/css/main.adb18095.chunk.css"
+  },
+  {
+    "revision": "7fae0e01c79f56f2f03f",
+    "url": "/website/static/js/2.c08dd0af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.c08dd0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f431c9fd3b9892595c",
+    "url": "/website/static/js/main.14676ba0.chunk.js"
+  },
+  {
+    "revision": "a50d945060eddc6481b5",
+    "url": "/website/static/js/runtime-main.e471a7fb.js"
+  },
+  {
+    "revision": "49a3dbf936b6a5884f674a10765c6499",
+    "url": "/website/static/media/Deep Learning Lab.49a3dbf9.png"
+  },
+  {
+    "revision": "41431c6ba8a37f4a426d96b870436959",
+    "url": "/website/static/media/Machine Learning for disambiguation of scientific article authors.41431c6b.png"
+  },
+  {
+    "revision": "f2976ba46f3cdd3c72a856026edf7102",
+    "url": "/website/static/media/Machine Learning.f2976ba4.gif"
+  },
+  {
+    "revision": "8195fd5473ee00d54a008e8c27573d5a",
+    "url": "/website/static/media/MeHome.8195fd54.png"
+  },
+  {
+    "revision": "cf67c8be93a56b1645286302b1fcc4af",
+    "url": "/website/static/media/NannySearch.cf67c8be.png"
+  },
+  {
+    "revision": "7378cb9f1d00375cb19d5fc991a784e4",
+    "url": "/website/static/media/SmartBin.7378cb9f.png"
+  },
+  {
+    "revision": "d8661b503187b1b9f5de3109dbb396bb",
+    "url": "/website/static/media/Tiforma.d8661b50.png"
+  },
+  {
+    "revision": "24c1ace60c7dda3ac2538fa5cfcb89ad",
+    "url": "/website/static/media/army.24c1ace6.png"
+  },
+  {
+    "revision": "a889c1e4c3a79a344e8f0fa4b0bf586c",
+    "url": "/website/static/media/cpc.a889c1e4.jpg"
+  },
+  {
+    "revision": "9c372ea93b197fc143ef2ec07dfe0377",
+    "url": "/website/static/media/linkedin.9c372ea9.png"
+  },
+  {
+    "revision": "a3fd0ad098b8ef82b973f2789fefa727",
+    "url": "/website/static/media/supsi.a3fd0ad0.png"
+  },
+  {
+    "revision": "a9ae42ad7b68e2039c33b778b9edfd8d",
+    "url": "/website/static/media/tds.a9ae42ad.png"
+  },
+  {
+    "revision": "a7fe36e08f28a6957ad0f3a0a75d9091",
+    "url": "/website/static/media/usi.a7fe36e0.png"
+  }
+]);
