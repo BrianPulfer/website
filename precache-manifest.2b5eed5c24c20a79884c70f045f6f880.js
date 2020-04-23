ecf9c40495996556dae1b58d571a7aec41828aa0
BrianPulfer <pulfer.brian21@gmail.com>
2020-04-24 00:36:35 +0200

    Updates

---
+++ precache-manifest.2b5eed5c24c20a79884c70f045f6f880.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1514596fb5a436cf6e23f4fe5b53ea9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb093b16060b3023b18",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "47fc0e656e3e85aa635a",
+    "url": "/static/css/main.adb18095.chunk.css"
+  },
+  {
+    "revision": "6fb093b16060b3023b18",
+    "url": "/static/js/2.e7b01461.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e7b01461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fc0e656e3e85aa635a",
+    "url": "/static/js/main.f6cd337c.chunk.js"
+  },
+  {
+    "revision": "5aa8c4c68520706df100",
+    "url": "/static/js/runtime-main.d1594a52.js"
+  },
+  {
+    "revision": "49a3dbf936b6a5884f674a10765c6499",
+    "url": "/static/media/Deep Learning Lab.49a3dbf9.png"
+  },
+  {
+    "revision": "41431c6ba8a37f4a426d96b870436959",
+    "url": "/static/media/Machine Learning for disambiguation of scientific article authors.41431c6b.png"
+  },
+  {
+    "revision": "f2976ba46f3cdd3c72a856026edf7102",
+    "url": "/static/media/Machine Learning.f2976ba4.gif"
+  },
+  {
+    "revision": "8195fd5473ee00d54a008e8c27573d5a",
+    "url": "/static/media/MeHome.8195fd54.png"
+  },
+  {
+    "revision": "cf67c8be93a56b1645286302b1fcc4af",
+    "url": "/static/media/NannySearch.cf67c8be.png"
+  },
+  {
+    "revision": "7378cb9f1d00375cb19d5fc991a784e4",
+    "url": "/static/media/SmartBin.7378cb9f.png"
+  },
+  {
+    "revision": "d8661b503187b1b9f5de3109dbb396bb",
+    "url": "/static/media/Tiforma.d8661b50.png"
+  },
+  {
+    "revision": "24c1ace60c7dda3ac2538fa5cfcb89ad",
+    "url": "/static/media/army.24c1ace6.png"
+  },
+  {
+    "revision": "a889c1e4c3a79a344e8f0fa4b0bf586c",
+    "url": "/static/media/cpc.a889c1e4.jpg"
+  },
+  {
+    "revision": "9c372ea93b197fc143ef2ec07dfe0377",
+    "url": "/static/media/linkedin.9c372ea9.png"
+  },
+  {
+    "revision": "a3fd0ad098b8ef82b973f2789fefa727",
+    "url": "/static/media/supsi.a3fd0ad0.png"
+  },
+  {
+    "revision": "a9ae42ad7b68e2039c33b778b9edfd8d",
+    "url": "/static/media/tds.a9ae42ad.png"
+  },
+  {
+    "revision": "a7fe36e08f28a6957ad0f3a0a75d9091",
+    "url": "/static/media/usi.a7fe36e0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.8e18891fae87925aaec31a3e238b70d5.js"
+  "/precache-manifest.2b5eed5c24c20a79884c70f045f6f880.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
