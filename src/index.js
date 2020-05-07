0b55704741d5c46fb52b0e9a6a01297669024e69
BrianPulfer <pulfer.brian21@gmail.com>
2020-05-07 15:11:34 +0200

    Fixed failed import of bootstrap in deployment environment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import './utilities/css/w3.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './bootstrap/dist/css/bootstrap.min.css'
 
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
