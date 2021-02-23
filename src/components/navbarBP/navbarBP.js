882eb8c7515efdff48f8383f1f521e68c7a70b0b
Brian Pulfer <42559439+BrianPulfer@users.noreply.github.com>
2021-02-23 17:57:13 +0100

    Currently selected tab now is shown in green.

--- src/components/navbarBP/navbarBP.js
+++ src/components/navbarBP/navbarBP.js
@@ -9,17 +9,33 @@ const CAREER_PATH = process.env.PUBLIC_URL + '/#/Career';
 const PROJECTS_PATH = process.env.PUBLIC_URL + '/#/Projects';
 const CONTACTS_PATH = process.env.PUBLIC_URL + '/#/Contacts';
 
+const POSSIBLE_SELECTIONS = ["Me", "Career", "Projects", "Contacts"];
+const SELECTED = "selected";
+const NOT_SELECTED = "not-selected";
+const NO_SELECTION = () => [NOT_SELECTED, NOT_SELECTED, NOT_SELECTED, NOT_SELECTED];
+
 class NavbarBP extends React.Component {
 
     constructor(props) {
         super(props);
 
-
         this.toggleExpand = this.toggleExpand.bind(this);
         this.closeNav = this.closeNav.bind(this);
 
+        const splitHREF = window.location.href.split("/");
+        const addr = splitHREF[splitHREF.length - 1];
+        let initialSelection = NO_SELECTION();
+
+        initialSelection = initialSelection.map((elem, i) => {
+            if (addr === POSSIBLE_SELECTIONS[i]) {
+                return SELECTED;
+            }
+            return NOT_SELECTED;
+        });
+
         this.state = {
-            navExpanded: false
+            navExpanded: false,
+            selection: initialSelection
         }
     }
 
@@ -28,23 +44,36 @@ class NavbarBP extends React.Component {
         this.setState({navExpanded: newExpanded});
     }
 
-    closeNav() {
-        this.setState({navExpanded: false});
+    closeNav(address) {
+        const splitAddress = address.split("/");
+        const addr = splitAddress[splitAddress.length - 1];
+
+        let newSelection = NO_SELECTION();
+
+        for (let i = 0; i < POSSIBLE_SELECTIONS.length; i++) {
+            if (addr === POSSIBLE_SELECTIONS[i]) {
+                newSelection[i] = SELECTED;
+            }
+        }
+
+        this.setState({
+            navExpanded: false,
+            selection: newSelection
+        });
         window.scrollTo(0, 0);
     }
 
     render() {
-
         return (
             <Navbar expand="lg" onClick={this.toggleExpand} expanded={this.state.navExpanded}>
                 <Navbar.Brand className="BPBrand" href={HOME_PATH}>Brian Pulfer</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse>
                     <Nav className="ml-auto" onSelect={this.closeNav}>
-                        <Nav.Link href={ME_PATH}>Me</Nav.Link>
-                        <Nav.Link href={CAREER_PATH}>Career</Nav.Link>
-                        <Nav.Link href={PROJECTS_PATH}>Projects</Nav.Link>
-                        <Nav.Link href={CONTACTS_PATH}>Contacts</Nav.Link>
+                        <Nav.Link href={ME_PATH} className={this.state.selection[0]}>Me</Nav.Link>
+                        <Nav.Link href={CAREER_PATH} className={this.state.selection[1]}>Career</Nav.Link>
+                        <Nav.Link href={PROJECTS_PATH} className={this.state.selection[2]}>Projects</Nav.Link>
+                        <Nav.Link href={CONTACTS_PATH} className={this.state.selection[3]}>Contacts</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
