a2b1d79a2bfbff88d13393f07527c4bc8a9946ec
BrianPulfer <brianpulfer95@gmail.com>
2022-03-05 20:54:37 +0100

    Updated blog section.

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 import './blog.css';
-import Tweet from "../../components/tweet/tweet";
+import Post from "../../components/post/post";
+
+import vit from './imgs/vit.png';
 
 class Blog extends React.Component {
     constructor(props) {
         super(props);
     }
 
-
     render() {
         return (
             <div className={"blog-content"}>
-                <Tweet id={'1489272716918235145'} />
+                <Post
+                    title={'Vision Transformers from Scratch'}
+                    img={vit}
+                    description={"I implemented a simple version of Vision Transformers (ViT) from scratch in pytorch. Click on the above image to read the medium story."}
+                    link={"https://medium.com/mlearning-ai/vision-transformers-from-scratch-pytorch-a-step-by-step-guide-96c3313c2e0c"}
+                />
             </div>
         );
     }
