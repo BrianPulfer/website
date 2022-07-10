33d783835ab4314a014f84794191d2fc8ce94ea4
Brian Pulfer <Brian.Pulfer@unige.ch>
2022-07-10 14:25:40 +0200

    Added blog on DDPMs.

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -4,11 +4,19 @@ import './blog.css';
 import Post from "../../components/post/post";
 
 import vit from './imgs/vit.png';
+import ddpm from './imgs/ddpm.gif';
 
 class Blog extends React.Component {
     render() {
         return (
             <div className={"blog-content"}>
+                <Post
+                    title={'Generating images with DDPMs: A PyTorch Implementation'}
+                    img={ddpm}
+                    description={"I implemented the original 'Denoising Diffusion Probabilistic Models' paper by Ho et. al. I train a DDPM model from scratch on the MNIST and Fashion-MNIST datasets. Code is publicly available."}
+                    link={"https://medium.com/@brianpulfer/enerating-images-with-ddpms-a-pytorch-implementation-cef5a2ba8cb1"}
+                />
+
                 <Post
                     title={'Vision Transformers from Scratch'}
                     img={vit}
