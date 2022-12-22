2c4042c990f83cc733e82c4db9e9490fc52fe5a6
Brian Pulfer <brianpulfer95@gmail.com>
2022-12-22 09:41:41 +0100

    Updated awards section to look better.

--- src/pages/awards/awards.js
+++ src/pages/awards/awards.js
@@ -7,16 +7,18 @@ import "./awards.css";
 class Awards extends React.Component{
 
     render() {
+        const titleSize = "36px";
+        const paragraphSize = "26px";
         return (
             <div className={"awards-container"}>
                 <div className={"award"}>
-                    <BPTitle text={"Swiss Engineering Award (2022)"}/>
-                    <BPParagraph size={"20px"} text={"In 2022, I was awarded the 'best presentation award' from the Swiss Engineering Ticino foundation for my master thesis on self-driving cars."}/>
+                    <BPTitle classes={"awardTitle"} size={titleSize} text={"🥇 Swiss Engineering Award (2022)"}/>
+                    <BPParagraph size={paragraphSize} text={"In 2022, I was awarded the 'best presentation award' from the Swiss Engineering Ticino foundation for my master thesis on self-driving cars."}/>
                 </div>
 
                 <div className={"award"}>
-                    <BPTitle text={"SODESKA Scholarship (2021)"}/>
-                    <BPParagraph size={"20px"} text={"In 2021, while studying for my master in Artificial Intelligence, I won the SODESKA scholarship awarded to the top 5 swiss students of the University of Southern Switzerland in a master course."}/>
+                    <BPTitle classes={"awardTitle"} size={titleSize} text={"🥇 SODESKA Scholarship (2021)"}/>
+                    <BPParagraph size={paragraphSize} text={"In 2021, while studying for my master in Artificial Intelligence, I won the SODESKA scholarship awarded to the top 5 swiss students of the University of Southern Switzerland in a master course."}/>
                 </div>
             </div>
         );
