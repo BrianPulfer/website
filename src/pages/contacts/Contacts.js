3bb230bca30d05b3ebb0d06f5bafd0f26325c034
Brian Pulfer <brianpulfer95@gmail.com>
2023-01-12 22:54:08 +0100

    Minor visual improvement.

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -22,80 +22,82 @@ class Contacts extends React.Component {
           text={"Here are some ways you can can in touch with me."}
         />
 
-        <CardGroup className={"contacts row offset-xl-3"}>
-          <Card id={"mail"} className={"contact col col-xl-2"}>
-            <a href={"mailto:brianpulfer95@gmail.com"}>
-              <CardImg variant={"top"} src={Mail} />
-            </a>
-            <Card.Body>
-              <Card.Title>E-Mail</Card.Title>
-              <Card.Text>
-                Feel free to send me e-mails if you are interest in learning
-                more about who I am and what i do.
-              </Card.Text>
-              <Button
-                className={"contact-btn"}
-                href={"mailto:brianpulfer95@gmail.com"}
-              >
-                Send me an e-mail
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card id={"linkedin"} className={"contact col col-xl-2"}>
-            <a href={"https://www.linkedin.com/in/BrianPulfer/"}>
-              <CardImg variant={"top"} src={Linkedin} />
-            </a>
-            <Card.Body>
-              <Card.Title>LinkedIn</Card.Title>
-              <Card.Text>
-                Everything about my professional carreer can be found on my
-                LinkedIn profile.
-              </Card.Text>
-              <Button
-                className={"contact-btn"}
-                href={"https://www.linkedin.com/in/BrianPulfer/"}
-              >
-                Find me on LinkedIn
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card id={"twitter"} className={"contact col col-xl-2"}>
-            <a href={"https://twitter.com/PulferBrian21"}>
-              <CardImg variant={"top"} src={Twitter} />
-            </a>
-            <Card.Body>
-              <Card.Title>Twitter</Card.Title>
-              <Card.Text>
-                Usually I only tweet about my own major achievements and deep
-                thoughts.
-              </Card.Text>
-              <Button
-                className={"contact-btn"}
-                href={"https://twitter.com/PulferBrian21"}
-              >
-                Tweet @Brian
-              </Button>
-            </Card.Body>
-          </Card>
-          <Card id={"github"} className={"contact col col-xl-2"}>
-            <a href={"https://github.com/BrianPulfer"}>
-              <CardImg variant={"top"} src={GitHub} />
-            </a>
-            <Card.Body>
-              <Card.Title>GitHub</Card.Title>
-              <Card.Text>
-                Checkout some of my professional work and toy-projects, as well
-                as my contributions.
-              </Card.Text>
-              <Button
-                className={"contact-btn"}
-                href={"https://github.com/BrianPulfer"}
-              >
-                Find me on GitHub
-              </Button>
-            </Card.Body>
-          </Card>
-        </CardGroup>
+        <Row className={"justify-content-center"}>
+          <CardGroup className={"contacts col col-md-10 col-l-4"}>
+            <Card id={"mail"} className={"contact"}>
+              <a href={"mailto:brianpulfer95@gmail.com"}>
+                <CardImg variant={"top"} src={Mail} />
+              </a>
+              <Card.Body>
+                <Card.Title>E-Mail</Card.Title>
+                <Card.Text>
+                  Feel free to send me e-mails if you are interest in learning
+                  more about who I am and what i do.
+                </Card.Text>
+                <Button
+                  className={"contact-btn"}
+                  href={"mailto:brianpulfer95@gmail.com"}
+                >
+                  Send me an e-mail
+                </Button>
+              </Card.Body>
+            </Card>
+            <Card id={"linkedin"} className={"contact"}>
+              <a href={"https://www.linkedin.com/in/BrianPulfer/"}>
+                <CardImg variant={"top"} src={Linkedin} />
+              </a>
+              <Card.Body>
+                <Card.Title>LinkedIn</Card.Title>
+                <Card.Text>
+                  Everything about my professional carreer can be found on my
+                  LinkedIn profile.
+                </Card.Text>
+                <Button
+                  className={"contact-btn"}
+                  href={"https://www.linkedin.com/in/BrianPulfer/"}
+                >
+                  Find me on LinkedIn
+                </Button>
+              </Card.Body>
+            </Card>
+            <Card id={"twitter"} className={"contact"}>
+              <a href={"https://twitter.com/PulferBrian21"}>
+                <CardImg variant={"top"} src={Twitter} />
+              </a>
+              <Card.Body>
+                <Card.Title>Twitter</Card.Title>
+                <Card.Text>
+                  Usually I only tweet about my own major achievements and deep
+                  thoughts.
+                </Card.Text>
+                <Button
+                  className={"contact-btn"}
+                  href={"https://twitter.com/PulferBrian21"}
+                >
+                  Tweet @Brian
+                </Button>
+              </Card.Body>
+            </Card>
+            <Card id={"github"} className={"contact"}>
+              <a href={"https://github.com/BrianPulfer"}>
+                <CardImg variant={"top"} src={GitHub} />
+              </a>
+              <Card.Body>
+                <Card.Title>GitHub</Card.Title>
+                <Card.Text>
+                  Checkout some of my professional work and toy-projects, as
+                  well as my contributions.
+                </Card.Text>
+                <Button
+                  className={"contact-btn"}
+                  href={"https://github.com/BrianPulfer"}
+                >
+                  Find me on GitHub
+                </Button>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Row>
       </div>
     );
   }
