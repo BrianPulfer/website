6e299c76e93106f6cc408ba4cf55b2f4fddd9525
Brian Pulfer <me@brianpulfer.ch>
2023-09-24 22:58:44 +0200

    Using Google Analytics 4.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
+
+import Head from "next/head"
+import Script from "next/script"
 import type { AppProps } from 'next/app'
+
 
 import theme from "../styles/theme"
 import { ChakraProvider } from '@chakra-ui/react'
@@ -7,16 +11,42 @@ import { ChakraProvider } from '@chakra-ui/react'
 import "@fontsource/raleway/400.css";
 import "@fontsource/open-sans/700.css";
 
-import "../styles/globals.css";
 
-
-export default function App({ Component, pageProps }: { Component: any, pageProps: AppProps }) {
+export default function App({ Component, pageProps }: { Component: any, pageProps: AppProps}) {
   const getLayout = Component.getLayout || ((page: React.ReactElement) => page)
   const content = getLayout(<Component {...pageProps} />)
 
+  // Initializing Google Analytics
+  const GA_ID = "G-2KSCVRRZHY"
+
+
+
   return (
-    <ChakraProvider theme={theme}>
-      {content}
-    </ChakraProvider>
+    <>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_ID}', {
+                      page_path: window.location.pathname,
+                      });
+                  `}
+        </Script>
+
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+
+      <ChakraProvider theme={theme}>
+        {content}
+      </ChakraProvider>
+    </>
   )
 }
+ 
